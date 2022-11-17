5566e4177d2f5f44a1a63c69dd77f3d15a9e1c0d
kialuna <77881537+kialuna@users.noreply.github.com>
2022-11-17 11:22:09 +0000

    Part 1 Complete
    Basic containers and state

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,36 @@
+import React from "react"
+import Header from "./Header";
+import TodoList from "./TodoList";
+class TodoContainer extends React.Component {
+    state = {
+        todos: [
+          {
+            id: 1,
+            title: "Setup development environment",
+            completed: true
+          },
+          {
+            id: 2,
+            title: "Develop website and add content",
+            completed: false
+          },
+          {
+            id: 3,
+            title: "Deploy to live server",
+            completed: false
+          }
+        ]
+       };
+
+
+    render() {
+        return (
+            <>
+                <Header/>
+                <TodoList todos={this.state.todos}/>
+            </>
+        )
+    };
+};
+
+export default TodoContainer
