1fbbd67ffac8994c7c10228ab05654444c640772
kialuna <77881537+kialuna@users.noreply.github.com>
2022-11-17 17:01:21 +0000

    Part 4 Complete

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -59,17 +59,31 @@ class TodoContainer extends React.Component {
         });
     };
 
+    setUpdate = (updatedTitle, id) => {
+        this.setState({
+            todos: this.state.todos.map(todo => {
+                if (todo.id === id){
+                    todo.title=updatedTitle
+                }
+                return todo
+            })
+        })
+    }
+
     render() {
         return (
             <div className="container">
                 <div className="inner">
                     <Header/>
                     <InputTodo
-                        addTodoProps={this.addTodoItem}/>
+                        addTodoProps={this.addTodoItem}
+                    />
                     <TodoList 
                         todos={this.state.todos} 
                         handleChangeProps={this.handleChange}
-                        deleteTodoProps={this.delTodo}/>
+                        deleteTodoProps={this.delTodo}
+                        setUpdate={this.setUpdate}
+                    />
                 </div>    
             </div>
         )
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,6 +2,22 @@ import React from "react";
 import styles from "./TodoItem.module.css"
 
 class TodoItem extends React.Component {
+    state= {
+        editing: false,
+    }
+
+    handleEditing=() => {
+        this.setState({
+            editing:true,
+        })
+    }
+
+    handleEditingDone = e => {
+        if (e.key === "Enter"){
+            this.setState({editing:false})
+        }
+    }
+
     render() {
         const completedStyle= {
             fontStyle: "italic",
@@ -12,8 +28,17 @@ class TodoItem extends React.Component {
 
         const { completed, id, title } = this.props.todo
 
+        let viewMode = {}
+        let editMode = {}
+
+        if (this.state.editing) {
+            viewMode.display="none"
+        } else {
+            editMode.display = "none"
+        }
         return (
             <li className={styles.item}>
+                <div onDoubleClick={this.handleEditing} style={viewMode}>
                 <input 
                     type="checkbox" 
                     className={styles.checkbox}
@@ -28,6 +53,18 @@ class TodoItem extends React.Component {
                 <span style={completed ? completedStyle : null}>
                    {title} 
                 </span>
+                </div>
+                <input 
+                    type="text" 
+                    style={editMode}
+                    className={styles.textInput}                     
+                    defaultValue={title}
+                    onChange={e => {
+                        this.props.setUpdate(e.target.value, id)
+                            }}
+                    onKeyDown={this.handleEditingDone}
+                />
+                
                 
             </li>
         )
