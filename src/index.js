6a373862847cdbb83496462b97ca72338f9123f6
kialuna <77881537+kialuna@users.noreply.github.com>
2022-11-21 11:22:44 +0000

    3/4 way through part 9

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import React from "react"
 import ReactDOM from 'react-dom'
-// Component file
-import TodoContainer from "./functionBased/components/TodoContainer"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+// Component files
+import TodoContainer from "./components/TodoContainer"
+import About from './pages/About'
+import NotMatch from './pages/NotMatch'
+import Navbar from "./pages/Navbar"
+
 // Stylesheet
-import "./functionBased/App.css"
+import "./App.css"
 
 
 ReactDOM.render(
    <React.StrictMode>
-    <TodoContainer />
-    </React.StrictMode>,
-     document.getElementById('root')
+      <BrowserRouter>
+      <Navbar />
+         <Routes>
+            <Route path='/' element={<TodoContainer />}></Route>
+            <Route path="about/*" element={<About/>}></Route>
+            <Route path='*' element={<NotMatch />}></Route>
+         </Routes>
+      </BrowserRouter>
+
+   </React.StrictMode>,
+   document.getElementById('root')
 )
 
---
+++ src/pages/About.js
@@ -0,0 +1,24 @@
+import React from "react"
+import { Routes, Route } from "react-router-dom"
+import SinglePage from "./SinglePage"
+
+const About = () => {
+
+  return (
+    <div>
+      This is an about page 
+    {/* <ul>
+        <li>
+            <Link to="/about-app">About App</Link>
+        </li>
+        <li>
+            <Link to="/about-author">About Author</Link>
+        </li>
+    </ul>
+    <Routes>
+        <Route path=":slug" element={<SinglePage/>}/>
+    </Routes> */}
+</div>
+  )
+}
+export default About
