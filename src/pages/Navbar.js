c70c80225abc11e8550a928c3f7dd8a2dc370c8b
kialuna <77881537+kialuna@users.noreply.github.com>
2022-11-22 16:15:16 +0000

    Halfway through part 10

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,8 +1,9 @@
-import React from "react"
+import React, {useState} from "react"
 import { NavLink } from "react-router-dom"
 
 
 const Navbar = () => {
+  const [navbarOpen, setNavbarOpen]=useState(false)
   const links = [
     {
       id: 1,
@@ -15,12 +16,22 @@ const Navbar = () => {
       text: "About",
     },
   ]
+  const handleToggle=()=>{
+    setNavbarOpen(prev => !prev)
+  }
+  const closeMenu = () => {
+    setNavbarOpen(false)
+  }
   return (
     <nav className="navBar">
-      <ul>
+      <button onClick={handleToggle}>{navbarOpen ? "Close": "Open"}</button>
+      <ul className={`menuNav${navbarOpen ? " showMenu" : ""}`}>
         {links.map(link => {
           return <li key={link.id}>
-            <NavLink to={link.path} className={({ isActive }) => isActive ? "active-link" : undefined}>
+            <NavLink 
+            to={link.path} 
+            className={({ isActive }) => isActive ? "active-link" : undefined}
+            onClick={()=> closeMenu()}>
               {link.text}
             </NavLink>
           </li>
